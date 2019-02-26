842b4f5e509f9fe79fa8cd34ad2d9d03210a0c13
Tanner Gildea <tanner@Tanners-MacBook-Pro.local>
2019-02-26 14:05:12 -0500

    more updates

--- class_2019-02-26.R
+++ class_2019-02-26.R
@@ -4,10 +4,13 @@ library(ggplot2)
 library(janitor)
 library(readxl)
 
-enrollment_data <- read_excel("class_enrollment_summary_by_term_2.26.19.xlsx", skip = 3)
+enrollment_data_2019 <- read_excel("class_enrollment_summary_by_term_2.26.19.xlsx", skip = 3) %>% 
+  clean_names() %>% 
+  filter(! is.na(course_name))
 
-enrollment_data <- clean_names(enrollment_data) %>% 
-  select(!na.rm(course_name))
+enrollment_data_2018 <- read_excel("class_enrollment_summary_by_term_03.06.18.xlsx", skip = 3) %>% 
+  clean_names() %>% 
+  filter(! is.na(course_name))
 
-#remove where course_name is NA
-View(enrollment_data)
+View(enrollment_data_2019)
+View(enrollment_data_2018)
