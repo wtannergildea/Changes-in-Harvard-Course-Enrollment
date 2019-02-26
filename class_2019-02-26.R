3eb50be83c4098d92b9fcdae59f1aa6540e4ecff
Tanner Gildea <tanner@Tanners-MacBook-Pro.local>
2019-02-26 14:42:57 -0500

    cleaned data

--- class_2019-02-26.R
+++ class_2019-02-26.R
@@ -12,5 +12,13 @@ enrollment_data_2018 <- read_excel("class_enrollment_summary_by_term_03.06.18.xl
   clean_names() %>% 
   filter(! is.na(course_name))
 
-View(enrollment_data_2019)
-View(enrollment_data_2018)
+# number, name, 2018 enrollment, 2019 enrollment, arrange(desc(change))
+
+joined_enrollment <- left_join(enrollment_data_2018, enrollment_data_2019, by = "course_id", suffix = c(".2018", ".2019")) %>% 
+  filter(! is.na(total.2019)) %>% 
+  mutate(enrollment_difference = (u_grad.2019 - u_grad.2018)) %>% 
+  select(course_title.2018, course_name.2018, u_grad.2018, u_grad.2019, enrollment_difference) %>% 
+  arrange(enrollment_difference) %>% 
+  slice(1:10)
+
+View(joined_enrollment)
